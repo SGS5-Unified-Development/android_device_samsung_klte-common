75821bc7e4d98f5d07667df7db72ff6f1905db86
Bruno Martins <bgcngm@gmail.com>
2019-11-03 11:30:05 +0200

    klte-common: Set shipping API level in the common product makefile
    It's not meant to be set as a board config flag. Simply do it by
    inheriting the makefile that now also exists for devices launched
    with KitKat.
    
    Change-Id: I1ba570823a902cbd352be831b15e64c0eed63044

--- klte.mk
+++ klte.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2014-2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -111,6 +111,7 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
 
 # Get non-open-source specific aspects
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 $(call inherit-product-if-exists, vendor/samsung/klte-common/klte-common-vendor.mk)
 
 # common msm8974
